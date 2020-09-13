efd87683bf9c711a40b79f7ae40280d07bc395b4
JeevanjotS <jeevanjot97@gmail.com>
2020-09-13 14:12:26 -0400

    Add basic demo for game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private Renderer rend;
+    public GameObject winText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+        winText.SetActive(false);
+    }
+
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.CompareTag("Pickup"))
+        {
+            c.gameObject.SetActive(false);
+            rend.material = c.gameObject.GetComponent<MeshRenderer>().material;
+            if (rend.material.name.Replace(" (Instance)", "") == "Phasing")
+            {
+                gameObject.GetComponent<PlayerJump>().enabled = false;
+                gameObject.layer = LayerMask.NameToLayer("Phasable");
+            }
+            else if(rend.material.name.Replace(" (Instance)", "") == "Jumping")
+            {
+                gameObject.GetComponent<PlayerJump>().enabled = true;
+                gameObject.layer = LayerMask.NameToLayer("Default");
+            }
+            else
+            {
+                gameObject.GetComponent<PlayerJump>().enabled = false;
+                gameObject.layer = LayerMask.NameToLayer("Default");
+            }
+        }
+        else if(c.gameObject.CompareTag("EndFlag"))
+        {
+            winText.SetActive(true);
+        }
+
+    }
+    void Update()
+    {
+        var kb = Keyboard.current;
+        if (kb.spaceKey.isPressed)
+        {
+            print("Jump Key Pressed!");
+        }
+        else
+        {
+            print("");
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJump : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpPower = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var kb = Keyboard.current;
+        if (kb.spaceKey.isPressed)
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+
+    }
+}
