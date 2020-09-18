2dbdf78b22a294c07f62d4865a7eba4bf9817526
Kevin <kevingeo971@gmail.com>
2020-09-18 11:54:06 -0400

    Jump only when on groud

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerJump : MonoBehaviour
 {
     private Rigidbody rb;
+    private Collider col;
     public float jumpPower = 10;
+    private double distanceToGround;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        distanceToGround = col.bounds.extents.y;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // print(this.name);
         var kb = Keyboard.current;
         if (kb.spaceKey.isPressed)
         {
-            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            if(Physics.Raycast(transform.position, - Vector3.up, (float)(distanceToGround + 0.1f)))
+            {
+                rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);   
+            }
         }
 
     }
