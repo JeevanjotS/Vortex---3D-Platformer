2834d09283f873e44fe91347d316b43150cdd4b5
Kevin <kevingeo971@gmail.com>
2020-10-09 17:37:49 -0400

    Added Speed up prefab, adjusted jump and added DefaultPowers to PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,12 +12,16 @@ public class PlayerController : MonoBehaviour
     private float movementY;
     private Renderer rend;
     public GameObject winText;
+    private float default_forward_speed = 0;
+    private float default_rotate_speed = 0;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rend = GetComponent<Renderer>();
         winText.SetActive(false);
+        default_forward_speed = forward_speed;
+        default_rotate_speed = rotate_speed;
     }
 
     void OnMove(InputValue movementValue)
@@ -35,40 +39,61 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0,movementX * rotate_speed,0);
     }
 
+    //Function to set everything to default
+    void DefaultPower()
+    {
+        gameObject.GetComponent<PlayerJump>().enabled = false;
+        gameObject.layer = LayerMask.NameToLayer("Default");
+        
+        // 0 - Default Layer
+        // 4 - Water Layer  
+        Physics.IgnoreLayerCollision(0, 4, true);
+
+        // Set forward_speed and rotate speed to original values
+        forward_speed = default_forward_speed;
+        rotate_speed = default_rotate_speed;
+    }
+
     private void OnTriggerEnter(Collider c)
     {
         if (c.gameObject.CompareTag("Pickup"))
         {
             c.gameObject.SetActive(false);
             rend.material = c.gameObject.GetComponent<MeshRenderer>().material;
+            DefaultPower();
             if (rend.material.name.Replace(" (Instance)", "") == "Phasing")
             {
-                gameObject.GetComponent<PlayerJump>().enabled = false;
+                //gameObject.GetComponent<PlayerJump>().enabled = false;
                 gameObject.layer = LayerMask.NameToLayer("Phasable");
                 
                 // 0 - Default Layer
                 // 4 - Water Layer  
-                Physics.IgnoreLayerCollision(0, 4, true);
+                // Physics.IgnoreLayerCollision(0, 4, true);
             }
             else if(rend.material.name.Replace(" (Instance)", "") == "Jumping")
             {
                 gameObject.GetComponent<PlayerJump>().enabled = true;
-                gameObject.layer = LayerMask.NameToLayer("Default");
-                Physics.IgnoreLayerCollision(0, 4, true);
+                // gameObject.layer = LayerMask.NameToLayer("Default");
+                // Physics.IgnoreLayerCollision(0, 4, true);
             }
             else if(rend.material.name.Replace(" (Instance)", "") == "Freezing")
             {
                 print(" Picked Up Freezing ");
-                gameObject.GetComponent<PlayerJump>().enabled = false;
-                gameObject.layer = LayerMask.NameToLayer("Default");
+                // gameObject.GetComponent<PlayerJump>().enabled = false;
+                // gameObject.layer = LayerMask.NameToLayer("Default");
                 Physics.IgnoreLayerCollision(0, 4, false);
             }
-            else
+            else if(rend.material.name.Replace(" (Instance)", "") == "SpeedUp")
             {
-                gameObject.GetComponent<PlayerJump>().enabled = false;
-                gameObject.layer = LayerMask.NameToLayer("Default");
-                Physics.IgnoreLayerCollision(0, 4, true);
+                forward_speed+=25;
+                rotate_speed+=10;
             }
+            // else
+            // {
+            //     gameObject.GetComponent<PlayerJump>().enabled = false;
+            //     gameObject.layer = LayerMask.NameToLayer("Default");
+            //     Physics.IgnoreLayerCollision(0, 4, true);
+            // }
         }
         else if(c.gameObject.CompareTag("EndFlag"))
         {
