fd710dde9610237eaf28a664de98b00d3f1868a6
Kevin <kevingeo971@gmail.com>
2020-11-04 20:25:54 -0500

    Added particles to Slime, Increase slime animation radius, increased spaceship size

--- Assets/Prefabs/Slime_Prefab/CollectableSlime.cs
+++ Assets/Prefabs/Slime_Prefab/CollectableSlime.cs
@@ -6,11 +6,18 @@ using UnityEngine;
 
 public class CollectableSlime : MonoBehaviour
 {
+
+    // void Start()
+    // {
+    //     gameObject.transform.parent.gameObject
+    // }
+
     void OnTriggerEnter(Collider c)
     {
         // print("Here");
         if (c.attachedRigidbody)
         {   
+            gameObject.transform.parent.gameObject.SetActive(false);
             this.gameObject.SetActive(false);
         }
     }
