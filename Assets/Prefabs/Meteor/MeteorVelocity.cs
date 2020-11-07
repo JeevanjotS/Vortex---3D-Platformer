2e9969c9b9b376c4f159cf8b2bd17efa4c2ce950
Kevin <kevingeo971@gmail.com>
2020-11-07 18:05:50 -0500

    Created Meteor Prefab

---
+++ Assets/Prefabs/Meteor/MeteorVelocity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorVelocity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime ); 
+    }
+}
