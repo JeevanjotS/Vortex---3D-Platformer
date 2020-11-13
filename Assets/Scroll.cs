3e8e815afbb92b92f67884328eab807a0ab9b64a
Jeevanjot <jeevanjot97@gmail.com>
2020-11-13 18:21:28 -0500

    Add Backstory and revert font changes to tutorial

---
+++ Assets/Scroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class Scroll : MonoBehaviour
+{
+    public float scrollSpeed = 20;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        Vector3 localVectorUp = transform.TransformDirection(0, 1, 0);
+        pos += localVectorUp * scrollSpeed * Time.deltaTime;
+        transform.position = pos;
+        if (Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+}
