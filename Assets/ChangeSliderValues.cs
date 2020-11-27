27550ba2e6144322621fb030ec5f7a37716640db
Kevin <kevingeo971@gmail.com>
2020-11-26 22:43:40 -0500

    Volume changes remain same accross levels, enemy sound increased

---
+++ Assets/ChangeSliderValues.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSliderValues : MonoBehaviour
+{
+
+    public Slider Background_music_slider;
+    public Slider SFX_slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Background_music_slider.value = PlayerPrefs.GetFloat("Background_music_volume");
+        SFX_slider.value = PlayerPrefs.GetFloat("SFX_volume");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/NextLevel.cs
+++ Assets/NextLevel.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextLevel : MonoBehaviour
 {
+
+    public Slider Background_music_slider;
+    public Slider SFX_slider;
+
     public void LoadMenu()
     {
         Time.timeScale = 1f;
@@ -14,6 +19,8 @@ public class NextLevel : MonoBehaviour
 
     public void Proceed()
     {
+        PlayerPrefs.SetFloat("Background_music_volume", Background_music_slider.value);
+        PlayerPrefs.SetFloat("SFX_volume", SFX_slider.value);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
