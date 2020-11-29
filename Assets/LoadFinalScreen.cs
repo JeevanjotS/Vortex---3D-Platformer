faf5499fa3ff946c861ebd978dc90ad0298c4a8f
Kevin <kevingeo971@gmail.com>
2020-11-28 23:23:39 -0500

    Added End screen, Level 2 astroid placement, Level 2 Floor fixes, Changed Box material

---
+++ Assets/LoadFinalScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadFinalScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.CompareTag("Player"))
+        {
+            FindObjectOfType<SoundManagerScript>().PlayEndLevel();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            ScoreScript.scoreValue = 0;
+        }
+    }
+}
